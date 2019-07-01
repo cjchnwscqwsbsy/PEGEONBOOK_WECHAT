6825e3ccb468af86ebb6183c0bf0638b66191dda
yychen6 <yychen6@amarsoft.com>
2019-07-01 15:07:46 +0800

    c

---
+++ component/budget_model_row/budget_model_row_init/budget_model_row_detail/budget_model_row_detail.js
@@ -0,0 +1,23 @@
+// component/budget_model_row/budget_model_row_init/budget_model_row_detail/budget_model_row_detail.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
